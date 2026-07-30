735c911cd4c829e3f41798d29ea884d280ad4817
MAYARA GONCALVES DE LIMA <00001118629334sp@aluno.educacao.sp.gov.br>
2026-07-30 17:58:05 -0300

    Terceiro commit, seta para voltar ao topo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log("Projeto Multidisciplinar iniciado.");
+const btnTopo = document.getElementById("btnTopo");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        btnTopo.classList.add("show");
+
+    }else{
+
+        btnTopo.classList.remove("show");
+
+    }
+
+});
+
+btnTopo.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
